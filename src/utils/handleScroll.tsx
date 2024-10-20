acdf52cd776bc1fdef227f111d3686d82e8ce117
Muking3 <marcmusasa04@gmail.com>
2024-10-20 15:13:59 +0200

    delete file

--- src/utils/handleScroll.tsx
+++
@@ -1,6 +0,0 @@
-export const handleScroll = (hasMore: boolean, loading: boolean, fetchData: any) => {
-    if (
-        window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 500 && hasMore && !loading) {
-        return fetchData();
-    }
-};
